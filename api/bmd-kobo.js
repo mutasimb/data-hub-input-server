be5927d8817e0f0051193667b4713f1ef945a149
Mutasim Billah <mutasim.wre@gmail.com>
2022-10-11 11:13:12 +0600

    bmd kobo old form files renamed

--- api/bmd-kobo.js
+++ api/bmd-kobo.js
@@ -2,16 +2,16 @@ const
   express = require("express"),
   router = express.Router(),
 
-  bmd_submission_to_entry_object = require("../libs/bmd_submission_to_entry_object"),
+  bmd_submission_to_entry_object_190520 = require("../libs/bmd_submission_to_entry_object_190520.js"),
 
-  BmdObservedData = require("../models/BmdObservedData");
+  BmdObservedDataV190520 = require("../models/BmdObservedDataV190520.js");
 
 // /api/bmd-kobo/v190520/
 router.post("/v190520/", async (req, res) => {
   try {
     const
-      newObj = bmd_submission_to_entry_object(req.body),
-      newEntry = new BmdObservedData(newObj);
+      newObj = bmd_submission_to_entry_object_190520(req.body),
+      newEntry = new BmdObservedDataV190520(newObj);
 
     await newEntry.save();
 
